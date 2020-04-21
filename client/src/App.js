6853c6babf84b416b9016c51b773fb30540a2a16
fuston05 <fuston05@yahoo.com>
2020-04-21 16:51:33 -0400

    intiial commit, back and front end boilerplate, dependencies

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> ready to code!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/data/projects.js
@@ -0,0 +1,32 @@
+export const projects= [
+  {
+    id: 1,
+    name: 'the covid project_1',
+    description: 'the covid project_1 description text'
+  },
+  {
+    id: 2,
+    name: 'alpha project_2',
+    description: 'alpha project_2 description text'
+  },
+  {
+    id: 3,
+    name: 'project_3',
+    description: 'project_3 description text'
+  }, 
+  {
+    id: 4,
+    name: 'beta project_4',
+    description: 'beta project_4 description text'
+  },
+  {
+    id: 5,
+    name: 'zebra project_5',
+    description: 'zebra project_5 description text'
+  },
+  {
+    id: 6,
+    name: 'money project_6',
+    description: 'money project_6 description text'
+  }
+];
